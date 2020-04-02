d7fe52d99cae0df2d0bbb80df39265ecbf509ecb
Edimara Roberta Rosa <edimara.roberta.rosa@avanade.com>
2020-04-01 22:53:34 -0300

    add construtor que recebe a injecao de dependecia do request delegate

--- MeuMiddleware.cs
+++ MeuMiddleware.cs
@@ -3,4 +3,8 @@ using Microsoft.AspNetCore.Http;
 public class MeuMiddleware
 {
     private readonly RequestDelegate _next;
+    public MeuMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
 }
